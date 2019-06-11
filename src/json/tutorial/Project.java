1668038a67a0af5e94d8f8ed237d4d2848c90b7a
SteveKaranja <karanjastevey@gmail.com>
2019-06-11 14:50:04 +0300

    Update Project.java

--- src/json/tutorial/Project.java
+++ src/json/tutorial/Project.java
@@ -1,17 +1,17 @@
-package com.company;
+package json.tutorial;
 
-import com.google.gson.Gson;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+        import com.google.gson.Gson;
+        import org.json.simple.JSONArray;
+        import org.json.simple.JSONObject;
+        import org.json.simple.parser.JSONParser;
+        import org.json.simple.parser.ParseException;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.io.*;
+        import java.util.ArrayList;
+        import java.util.List;
+        import java.util.Scanner;
+        import java.io.*;
 
-public class JString{
+public class Project{
     public static int students = 0;             //keeps count of number of students created
     public static List<Student> listOfStudents; //List that stores all created students
 
@@ -28,13 +28,13 @@ public class JString{
             Scanner n = new Scanner(System.in);
             System.out.println("\nRegistration Details");
             regno = Integer.toString(students + 1);
-            System.out.print("First Name:\t");
+            System.out.print("First Name: ");
             fname = n.next();
-            System.out.print("Last Name:\t");
+            System.out.print("Last Name: ");
             lname = n.next();
-            System.out.print("Age:\t");
+            System.out.print("Age: ");
             age = n.next();
-            System.out.print("Gender:\t");
+            System.out.print("Gender: ");
             gender = n.next();
             System.out.println();
             students += 1;
@@ -56,8 +56,10 @@ public class JString{
         String json = new Gson().toJson(listOfStudents);    //convert the list of students to a json string
 
         try {
-            BufferedWriter br =new BufferedWriter(new FileWriter("students.json", false)); //Writes the json string to a file
-            br.write(json);
+            //BufferedWriter writes text to character output stream, buffering characters so as 
+            //to provide for the efficient writing of single characters, arrays, and strings.
+            BufferedWriter br =new BufferedWriter(new FileWriter("C:\\Users\\Steve Karanja\\Documents\\NetBeansProjects\\json-tutorial\\src\\json\\tutorial\\student.json", false)); //Writes the json string to a file
+            br.write(json); 
             br.close();
             System.out.println("\nSaved to File Successfully!");
             mainMenu();
@@ -77,15 +79,16 @@ public class JString{
         listOfStudents = new ArrayList<Student>();      //Initializes the list to store students
 
         //Check whether the file exists and whether it is empty
-        File file = new File("students.json");
+        File file = new File("C:\\Users\\Steve Karanja\\Documents\\NetBeansProjects\\json-tutorial\\src\\json\\tutorial\\student.json");
         if (file.length()==0) {
-            return listOfStudents;          //If the file is empty the methods terminates by returning the initialized list of students
+           
+            return listOfStudents;   //If the file is empty the methods terminates by returning the initialized list of students
         }
 
         //If the file is not empty it proceeds to get Student objects from the file and adds them to the list of students
         Reader read = null;
         try {
-            read = new FileReader("students.json");
+            read = new FileReader("C:\\Users\\Steve Karanja\\Documents\\NetBeansProjects\\json-tutorial\\src\\json\\tutorial\\student.json");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -93,7 +96,7 @@ public class JString{
 
         JSONParser parser = new JSONParser();
 
-        JSONArray jsonArray = null;         //Initialize a json Array to store the student objects read from the file
+        JSONArray jsonArray = null;    //Initialize a json Array to store the student objects read from the file
         try {
             jsonArray = (JSONArray) parser.parse(read);     //Populate the Array by reading the file
         } catch (IOException e) {
@@ -102,7 +105,7 @@ public class JString{
             e.printStackTrace();
         }
 
-        for (Object jsonObject : jsonArray) {                               //Fetch student details from the array one at a time
+        for (Object jsonObject : jsonArray) {              //Fetch student details from the array one at a time
             JSONObject student = (JSONObject) jsonObject;
             String regno = (String) student.get("regno");
             String fname = (String) student.get("fname");
@@ -127,10 +130,69 @@ public class JString{
         }
     }
 
+    //Search a given reg no and return its index position
+    public static int searchStudents(List<Student> listOfStudents, String regno) {
+        for (Student s : listOfStudents) {
+            if (s.regno.contentEquals(regno)) {
+                return listOfStudents.indexOf(s);
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
+    //Search and display the student details using a given reg no
+    public static void searchReg(List<Student> listOfStudents, String regno) {
+        int found = searchStudents(listOfStudents,regno);
+        if (found == Integer.MAX_VALUE) {
+            System.out.println("Does Not Exist");
+            return;
+        }
+
+        Student f = listOfStudents.get(found);
+        System.out.println("\nReg No:\t"+f.regno+"\nF Name:\t"+f.fname+"\nS Name:\t"+f.lname+"\nAge:\t"+f.age+"\nGender:\t"+f.gender);
+    }
+
+    //Delete the student at the given index
+    public static void deleteStudent(List<Student> listOfStudents, int index) {
+        if (index == Integer.MAX_VALUE) {
+            System.out.println("Student Does Not Exist");
+            return;
+        }
+        listOfStudents.remove(index);
+        System.out.println("Deleted Successfully!");
+        saveStudents(listOfStudents);
+    }
+
+    //Edit Details of student at given index
+    public static void editStudent(List<Student> listOfStudents, int index) {
+        if (index == Integer.MAX_VALUE) {
+            System.out.println("Student Does Not Exist");
+            return;
+        }
+        Scanner n = new Scanner(System.in);
+        Student s = listOfStudents.get(index);
+
+        System.out.println("Enter New Details:");
+        System.out.print("First Name: ");
+        String fname = n.next();
+        System.out.print("Last Name: ");
+        String lname = n.next();
+        System.out.print("Age: ");
+        String age = n.next();
+        System.out.print("Gender: ");
+        String gender = n.next();
+
+        Student edit = new Student(s.regno,fname,lname,age,gender);
+        listOfStudents.set(index,edit);
+        System.out.println("Edited Successfully");
+        saveStudents(listOfStudents);
+    }
+
+
     //Menu to add new student or to save the already added students
     public static void menu(){
         System.out.println("1) Add New\n2) Save\n");
-        System.out.print("Selection:\t");
+        System.out.print("Selection: ");
 
         Scanner s = new Scanner(System.in);
         switch (s.nextInt())
@@ -152,12 +214,15 @@ public class JString{
     }
 
     /* Main menu that allows:
-        * Addition of new students to existing records
-        * Displaying the students that are in the existing records
+     * Addition of new students to existing records
+     * Displaying the students that are in the existing records
+     * Search students
+     * Edit Students
+     * Delete Students
      */
     public static void mainMenu() {
-        System.out.println("\n1) Add student\n2) Display Students\n3) Exit");
-        System.out.print("Selection:\t");
+        System.out.println("\n1) Add student\n2) Display Students\n3) Search Student\n4) Edit Student\n5) Delete Student\n6) Exit");
+        System.out.print("Selection: ");
         Scanner s = new Scanner(System.in);
         switch (s.nextInt())
         {
@@ -168,13 +233,32 @@ public class JString{
                 break;
 
             case 2:
-                readStudents();
-                displayStudents(listOfStudents);
+                displayStudents(readStudents());
                 mainMenu();
                 break;
 
             case 3:
-                System.exit(1);
+                System.out.println("Enter Reg No to search: ");
+                searchReg(readStudents(),s.next());
+                mainMenu();
+                break;
+
+            case 4:
+                readStudents();
+                System.out.print("Enter Reg No to edit: ");
+                editStudent(listOfStudents,searchStudents(listOfStudents,s.next()));
+                mainMenu();
+                break;
+
+            case 5:
+                readStudents();
+                System.out.print("Enter Reg No to Delete: ");
+                deleteStudent(listOfStudents,searchStudents(listOfStudents,s.next()));
+                mainMenu();
+                break;
+
+            case 6:
+                System.exit(0);
                 break;
 
             default:
