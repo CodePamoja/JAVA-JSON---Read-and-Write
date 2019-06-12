9536703173b8c71ecc4d7830b8d015cd62fa1335
Eric Mureithi <eriqdev@gmail.com>
2019-06-12 09:46:25 +0300

    Project.java

--- src/json/tutorial/Project.java
+++ src/json/tutorial/Project.java
@@ -1,17 +1,21 @@
-package json.tutorial;
+/* Java Program that uses Google Gson and Simple Json Libraries to;
+* Create a Json String from a list of Student Objects
+* Save the Json String to a Json File
+* Parse the Json File to get, edit, add or delete the Student Objects
+*/
 
-        import com.google.gson.Gson;
-        import org.json.simple.JSONArray;
-        import org.json.simple.JSONObject;
-        import org.json.simple.parser.JSONParser;
-        import org.json.simple.parser.ParseException;
+import com.google.gson.Gson;                    //Used to Create the Json String from the List of Student Objects
+import org.json.simple.parser.JSONParser;       //Parse the Json File to get the Objects
+import org.json.simple.JSONArray;               //Store the the acquired Objects in an Array
+import org.json.simple.JSONObject;              //Acquire each Object individually 
+import org.json.simple.parser.ParseException;
 
-        import java.util.ArrayList;
-        import java.util.List;
-        import java.util.Scanner;
-        import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.io.*;
 
-public class Project{
+public class Project {
     public static int students = 0;             //keeps count of number of students created
     public static List<Student> listOfStudents; //List that stores all created students
 
@@ -28,13 +32,13 @@ public class Project{
             Scanner n = new Scanner(System.in);
             System.out.println("\nRegistration Details");
             regno = Integer.toString(students + 1);
-            System.out.print("First Name: ");
+            System.out.print("First Name:\t");
             fname = n.next();
-            System.out.print("Last Name: ");
+            System.out.print("Last Name:\t");
             lname = n.next();
-            System.out.print("Age: ");
+            System.out.print("Age:\t");
             age = n.next();
-            System.out.print("Gender: ");
+            System.out.print("Gender:\t");
             gender = n.next();
             System.out.println();
             students += 1;
@@ -53,16 +57,17 @@ public class Project{
     //Save the list of students to a json file
     public static void saveStudents(List<Student> listOfStudents) {
 
-        String json = new Gson().toJson(listOfStudents);    //convert the list of students to a json string
+        String json = new Gson().toJson(listOfStudents);                                //convert the list of students to a json string using the Gson Library
 
         try {
-            //BufferedWriter writes text to character output stream, buffering characters so as 
-            //to provide for the efficient writing of single characters, arrays, and strings.
-            BufferedWriter br =new BufferedWriter(new FileWriter("C:\\Users\\Steve Karanja\\Documents\\NetBeansProjects\\json-tutorial\\src\\json\\tutorial\\student.json", false)); //Writes the json string to a file
-            br.write(json); 
+            /* BufferedWriter writes text to character output stream, 
+               * Buffering characters to provide for the efficient writing of 
+               * Single characters, Arrays, and Strings.
+            */
+            BufferedWriter br =new BufferedWriter(new FileWriter("students.json", false)); //Writes the json string to a file
+            br.write(json);
             br.close();
             System.out.println("\nSaved to File Successfully!");
-            mainMenu();
         } catch(IOException e) {
             System.out.println("error submitted" + e);
         }
@@ -79,16 +84,15 @@ public class Project{
         listOfStudents = new ArrayList<Student>();      //Initializes the list to store students
 
         //Check whether the file exists and whether it is empty
-        File file = new File("C:\\Users\\Steve Karanja\\Documents\\NetBeansProjects\\json-tutorial\\src\\json\\tutorial\\student.json");
+        File file = new File("students.json");
         if (file.length()==0) {
-           
-            return listOfStudents;   //If the file is empty the methods terminates by returning the initialized list of students
+            return listOfStudents;                      //If the file is empty the methods terminates by returning the initialized list of students
         }
 
         //If the file is not empty it proceeds to get Student objects from the file and adds them to the list of students
         Reader read = null;
         try {
-            read = new FileReader("C:\\Users\\Steve Karanja\\Documents\\NetBeansProjects\\json-tutorial\\src\\json\\tutorial\\student.json");
+            read = new FileReader("students.json");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -96,7 +100,7 @@ public class Project{
 
         JSONParser parser = new JSONParser();
 
-        JSONArray jsonArray = null;    //Initialize a json Array to store the student objects read from the file
+        JSONArray jsonArray = null;                         //Initialize a json Array to store the student objects read from the file
         try {
             jsonArray = (JSONArray) parser.parse(read);     //Populate the Array by reading the file
         } catch (IOException e) {
@@ -105,7 +109,7 @@ public class Project{
             e.printStackTrace();
         }
 
-        for (Object jsonObject : jsonArray) {              //Fetch student details from the array one at a time
+        for (Object jsonObject : jsonArray) {                               //Fetch student details from the array one at a time
             JSONObject student = (JSONObject) jsonObject;
             String regno = (String) student.get("regno");
             String fname = (String) student.get("fname");
@@ -172,14 +176,14 @@ public class Project{
         Scanner n = new Scanner(System.in);
         Student s = listOfStudents.get(index);
 
-        System.out.println("Enter New Details:");
-        System.out.print("First Name: ");
+        System.out.println("\nEnter New Details:");
+        System.out.print("First Name:\t");
         String fname = n.next();
-        System.out.print("Last Name: ");
+        System.out.print("Last Name:\t");
         String lname = n.next();
-        System.out.print("Age: ");
+        System.out.print("Age:\t");
         String age = n.next();
-        System.out.print("Gender: ");
+        System.out.print("Gender:\t");
         String gender = n.next();
 
         Student edit = new Student(s.regno,fname,lname,age,gender);
@@ -187,12 +191,21 @@ public class Project{
         System.out.println("Edited Successfully");
         saveStudents(listOfStudents);
     }
-
+        
+    //Promts user to press enter to continue after each operation
+    public static  void promptEnterKey(){
+        System.out.print("\nPress \"ENTER\" to continue...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     //Menu to add new student or to save the already added students
     public static void menu(){
         System.out.println("1) Add New\n2) Save\n");
-        System.out.print("Selection: ");
+        System.out.print("Selection:\t");
 
         Scanner s = new Scanner(System.in);
         switch (s.nextInt())
@@ -204,6 +217,7 @@ public class Project{
 
             case 2:
                 saveStudents(listOfStudents);
+                promptEnterKey();
                 mainMenu();
                 break;
 
@@ -222,7 +236,7 @@ public class Project{
      */
     public static void mainMenu() {
         System.out.println("\n1) Add student\n2) Display Students\n3) Search Student\n4) Edit Student\n5) Delete Student\n6) Exit");
-        System.out.print("Selection: ");
+        System.out.print("Selection:\t");
         Scanner s = new Scanner(System.in);
         switch (s.nextInt())
         {
@@ -234,26 +248,30 @@ public class Project{
 
             case 2:
                 displayStudents(readStudents());
+                promptEnterKey();
                 mainMenu();
                 break;
 
             case 3:
-                System.out.println("Enter Reg No to search: ");
+                System.out.println("Enter Reg No to search:\t");
                 searchReg(readStudents(),s.next());
+                promptEnterKey();
                 mainMenu();
                 break;
 
             case 4:
                 readStudents();
-                System.out.print("Enter Reg No to edit: ");
+                System.out.print("Enter Reg No to edit:\t");
                 editStudent(listOfStudents,searchStudents(listOfStudents,s.next()));
+                promptEnterKey();
                 mainMenu();
                 break;
 
             case 5:
                 readStudents();
-                System.out.print("Enter Reg No to Delete: ");
+                System.out.print("Enter Reg No to Delete:\t");
                 deleteStudent(listOfStudents,searchStudents(listOfStudents,s.next()));
+                promptEnterKey();
                 mainMenu();
                 break;
 
